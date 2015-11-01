794b0b80ca99797abb48e5ec625fa4175e764de0
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-11-01 22:04:59 +0900

    implement proxy

---
+++ lib/http_proxy.ex
@@ -0,0 +1,53 @@
+defmodule HttpProxy do
+  use Plug.Builder
+  import Plug.Conn
+
+  @target "http://google.com"
+
+  plug Plug.Logger
+  plug :dispatch
+
+  def start(_argv) do
+    port = 4001
+    IO.puts "Running Proxy with Cowboy on http://localhost:#{port}"
+    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
+    :timer.sleep(:infinity)
+  end
+
+  def dispatch(conn, _opts) do
+    {:ok, client} = :hackney.request :get, uri(conn), conn.req_headers, :stream, []
+
+    conn
+    |> write_proxy(client)
+    |> read_proxy(client)
+  end
+
+  defp write_proxy(conn, client) do
+    case read_body(conn, []) do
+      {:ok, body, conn} ->
+        :hackney.send_body client, body
+        conn
+      {:more, body, conn} ->
+        :hackney.send_body client, body
+        write_proxy conn, client
+    end
+  end
+
+  defp read_proxy(conn, client) do
+    {:ok, status, headers, client} = :hackney.start_response client
+    {:ok, body} = :hackney.body client
+
+    headers = List.keydelete headers, "Transfer-Encoding", 0
+
+    %{conn | resp_headers: headers}
+    |> send_resp(status, body)
+  end
+
+  defp uri(conn) do
+    base = @target <> "/" <> Enum.join(conn.path_info, "/")
+    case conn.query_string do
+      "" -> base
+      qs -> base <> "?" <> qs
+    end
+  end
+end
