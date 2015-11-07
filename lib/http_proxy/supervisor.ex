b8919c6b2f02125e24eb49b23c2a25a2398d7687
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-11-07 22:02:26 +0900

    add error case

--- lib/http_proxy/supervisor.ex
+++ lib/http_proxy/supervisor.ex
@@ -10,12 +10,34 @@ defmodule HttpProxy.Supervisor do
   def init(:ok) do
     import Supervisor.Spec
 
-    @proxies
+    proxies?(@proxies)
     |> Enum.reduce([], fn proxy, acc ->
       module_name = "HttpProxy.Handle#{proxy.port}"
       [worker(HttpProxy.Handle, [[proxy, module_name]], [id: String.to_atom(module_name)]) | acc]
     end)
     |> supervise(strategy: :one_for_one)
-
   end
+
+  defp proxies?(nil) do
+    msg = ~s"""
+    You should set config/config.exs like the following lines.
+
+    ---
+    use Mix.Config
+
+    config :http_proxy,
+      proxies: [
+        %{port: 4000,
+          default_to: "http://google.com",
+          path: [
+            %{from: "", to: "http://yahoo.com"},
+            %{from: "neko", to: "http://yahoo.co.jp"}
+          ]
+          }
+        ]
+    ---
+    """
+    raise ArgumentError, msg
+  end
+  defp proxies?(proxies), do: proxies
 end
