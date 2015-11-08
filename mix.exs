5e52ce542143635052730211740e93692122cd04
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-11-08 13:41:17 +0900

    update a bit

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule HttpProxy.Mixfile do
 
   def project do
     [app: :http_proxy,
-     version: "0.0.1",
-     elixir: "~> 1.1",
+     version: "0.1.0",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
