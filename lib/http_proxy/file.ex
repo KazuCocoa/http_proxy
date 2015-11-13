33775d9a2e6a34a3d8d2fa00561b2cb226590244
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-11-14 00:53:50 +0900

    add read as json

--- lib/http_proxy/file.ex
+++ lib/http_proxy/file.ex
@@ -1,7 +1,8 @@
 defmodule HttpProxy.File do
   @moduledoc false
 
-  @export_path Application.get_env(:http_proxy, :export_path) || "default"
+  @export_path Application.get_env(:http_proxy, :export_path) || "default/__files"
+  @mapping_path Application.get_env(:http_proxy, :mapping_path) || "default/mappings"
 
   def filename(conn) do
     random_st = Integer.to_string(:random.uniform 100_00_00_00)
@@ -11,5 +12,15 @@ defmodule HttpProxy.File do
   def export(json, file) do
     unless File.exists?(@export_path), do: File.mkdir_p @export_path
     File.write((@export_path <> "/" <> file), json)
+  end
+
+  def read_from(file) do
+    unless File.exists?(@mapping_path), do: raise ArgumentError, "no mapping files"
+    case File.read (@mapping_path <> "/" <> file) do
+      {:ok, body} ->
+        Poison.decode body
+      {:error, message} ->
+        {:error, message}
+    end
   end
 end
