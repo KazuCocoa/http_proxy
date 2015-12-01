1ef8a888e7e66ce1d562c3179158519a325036d6
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-12-02 00:57:46 +0900

    add reading json files

--- lib/http_proxy/utils/file.ex
+++ lib/http_proxy/utils/file.ex
@@ -26,13 +26,35 @@ defmodule HttpProxy.Utils.File do
   end
 
   # TODO: read recorded files
-  def read_from(file) do
-    unless File.exists?(get_export_path), do: raise(ArgumentError, "no mapping files")
-    case File.read (get_export_path <> "/" <> file) do
-      {:ok, body} ->
-        JSX.decode body
-      {:error, message} ->
-        {:error, message}
+
+  def read_json_file!(path) do
+    case read_json_file(path) do
+      {:ok, body}       -> body
+      {:error, message} -> raise ArgumentError, message
+    end
+  end
+  def read_json_file(path) do
+    case File.read(path) do
+      {:ok, body}       -> JSX.decode(body)
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  def json_files!(dir) do
+    case json_files(dir) do
+      {:ok, files}      -> files
+      {:error, message} -> raise ArgumentError, message
+    end
+  end
+  def json_files(dir \\ ".") do
+    case File.ls(dir) do
+      {:ok, files} ->
+        files = files
+                |> Enum.filter_map(fn file ->
+                  Path.extname(file) == ".json"
+                end, &(dir <> "/" <> &1))
+        {:ok, files}
+      {:error, message} -> {:error, message}
     end
   end
 end
--- test/http_proxy/utils/http_proxy_file_test.exs
+++ test/http_proxy/utils/http_proxy_file_test.exs
@@ -12,4 +12,15 @@ defmodule HttpProxy.Utils.File.Test do
     assert HttpProxyFile.get_response_path == "test/data/__files"
     assert HttpProxyFile.get_mapping_path == "test/data/mappings"
   end
+
+  test "check read json files" do
+    json_test_dir = "test/data/mappings"
+    json_file_path = "test/data/mappings/sample.json"
+    expected_json = %{"response" => %{"body" => "<html>hello world</html>", "cookies" => %{},
+                      "headers" => %{"Content-Type" => "text/html; charset=UTF-8",
+                        "Server" => "GFE/2.0"}, "status_code" => 200}}
+
+    assert {:ok, [^json_file_path]} = HttpProxyFile.json_files(json_test_dir)
+    assert {:ok, expected_json} == HttpProxyFile.read_json_file(json_file_path)
+  end
 end
