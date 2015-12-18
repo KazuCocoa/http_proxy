cc75ad1d9c00e9082a2d7e445b6cd1d2683d732e
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-12-18 22:53:58 +0900

    bump up

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HttpProxy.Mixfile do
 
   def project do
     [app: :http_proxy,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.1",
      name: "ExHttpProxy",
      source_url: "https://github.com/KazuCocoa/http_proxy",
